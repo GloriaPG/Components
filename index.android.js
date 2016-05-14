f0fac8b180ccfa7b1b8c261c1f86ed38da775dcf
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-05-14 11:15:58 -0500

    GEO

--- index.android.js
+++ index.android.js
@@ -1,6 +1,7 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * @flow
  */
 
 import React, { Component } from 'react';
@@ -12,40 +13,58 @@ import {
 } from 'react-native';
 
 class Components extends Component {
+
+    constructor() {
+      super();
+      this.state = {
+          initialPosition: 'unknown',
+          lastPosition: 'unknown'
+      };
+
+      this.watchID = (null: ?number);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            var initialPosition = JSON.stringify(position);
+            this.setState({initialPosition})
+        },
+        (error) => alert(error.message),
+        {enableHighAccuracy: true, timeout: 20000, minimumAge: 1000}
+    );
+
+    this.watchId = navigator.geolocation.watchPosition((position) => {
+        var lastPosition = JSON.stringify(position);
+        this.setState({lastPosition});
+    });
+  }
+
+  componentWillUnmount() {
+      navigator.geolocation.clearWatch(this.watchId);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+            <View style={styles.main}>
+            <Text style={styles.title}>
+              {this.state.initialPosition}
+            </Text>
+            <Text style={styles.title}>
+              {this.state.lastPosition}
+            </Text>
+            </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    main: {
+        paddingTop: 20
+    },
+    title: {
+        fontWeight: '500'
+    }
 });
 
 AppRegistry.registerComponent('Components', () => Components);
